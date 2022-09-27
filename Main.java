90445e53d3298b29926597bd58bd6f98b291b5cc
AMAN1801 <68692647+AMAN1801@users.noreply.github.com>
2022-09-27 18:26:16 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main{    
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        num1 += num2;
+        num2 = num1-num2;
+        num1 -= num2;
+
+        System.out.println("First number is " + num1);
+        System.out.println("Second number is " + num2);
+
+        return;
+    }
+}
