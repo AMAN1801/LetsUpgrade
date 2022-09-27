82bd73f1dc88677fbe53627982f85f5cd9bbf170
AMAN1801 <68692647+AMAN1801@users.noreply.github.com>
2022-09-27 20:04:50 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,21 +1,12 @@
-import java.util.Scanner;
-
-class Main{    
-
+class Main{
     public static void main(String[] args) {
+        
+        int sum=0;
 
-        Scanner sc = new Scanner(System.in);
+        for(int i=1;i<=50;i++){
+            sum+=i;
+        }
 
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-
-        num1 += num2;
-        num2 = num1-num2;
-        num1 -= num2;
-
-        System.out.println("First number is " + num1);
-        System.out.println("Second number is " + num2);
-
-        return;
+        System.out.println(sum);
     }
 }
